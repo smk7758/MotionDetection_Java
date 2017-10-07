0ddb7f9ac854b1843c3ebb2b6ee122d041d369f8
smk7758 <maxkovko@yahoo.co.jp>
2017-10-08 03:19:40 +0900

    MotionDitection_Java - 0.0.2
    Cannot write.

--- src/com/github/smk7758/MotionDetection_Java/Main.java
+++ src/com/github/smk7758/MotionDetection_Java/Main.java
@@ -5,8 +5,9 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -36,43 +37,49 @@ public class Main {
 		} else if (!Files.isDirectory(outputfolder_path)) {
 			System.out.println("outputpath is not a folder.");
 		}
-		List<Path> inputfiles = null;
-		try {
-			inputfiles = Files.list(inputfolder_path).collect(Collectors.toList());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// inputfiles check
-		if (inputfiles == null) {
-			System.out.println("cant find the input files.");
-			return;
-		}
-		int inputfiles_size = inputfiles.size();
-		if (inputfiles_size == 0) {
-			System.out.println("there is any files in the inputfolder.");
-		}
-		VideoCapture vc = new VideoCapture(); // Like a Stream of Video
-		Mat mat_capture = new Mat(); // その時のframe
-		Mat mat_first = null; // 一番最初のframe(背景)
-		Mat mat_export = new Mat(); // 出力frame
-		boolean has_next_frame = true;
-		Path outputfile_path = null;
-		VideoWriter vw = new VideoWriter();
-		for (Path inputfile_path : inputfiles) {
-			outputfile_path = Paths.get(outputfolder_path.toString() + FileSystems.getDefault().getSeparator()
-					+ inputfile_path.getFileName().toString());// Using new thing from Java8: Path, Files, FileSystems
-			vc.open(inputfile_path.toString()); // Open
-			vc.read(mat_capture); // Read from vc to mat_capture.
-			if (mat_capture.empty()) {
-				System.out.println("cant read from video.");
-				break;
+		List<Path> inputfiles_list = new ArrayList<>(10);
+		try (Stream<Path> inputfiles = Files.list(inputfolder_path)) {
+			inputfiles.forEach(inputfile_path -> inputfiles_list.add(inputfile_path));
+
+			// inputfiles_list check
+			if (inputfiles_list.isEmpty()) {
+				System.out.println("there is any files in the inputfolder.?");
+				return;
 			}
-			mat_first = mat_capture.clone();
-			while (has_next_frame) {
-				has_next_frame = vc.read(mat_capture); // Re:read
-				if (!mat_capture.empty()) {
+
+			VideoCapture vc = new VideoCapture(); // Like a Stream of Video
+			Mat mat_capture = new Mat(); // その時のframe
+			Mat mat_first = null; // 一番最初のframe(背景)
+			Mat mat_export = new Mat(); // 出力frame
+			boolean has_next_frame = true;
+			Path outputfile_path = null;
+			VideoWriter vw = new VideoWriter();
+			for (Path inputfile_path : inputfiles_list) {
+				// output_path
+				outputfile_path = Paths.get(outputfolder_path.toString() + FileSystems.getDefault().getSeparator()
+						+ inputfile_path.getFileName().toString());// Using new thing from Java8: Path, Files, FileSystems
+
+				// VC
+				vc.open(inputfile_path.toString()); // open
+				vc.read(mat_capture); // Read from vc to mat_capture.
+
+				// Output video
+				int codec = VideoWriter.fourcc('M', 'P', '4', 'S');
+				double fps = vc.get(5);
+				Size size = new Size(vc.get(3), vc.get(4));
+				vw.open(outputfile_path.toString(), codec, fps, size);
+				System.out.println("Output: " + outputfile_path.toString());
+
+				// mat copy
+				mat_first = mat_capture.clone();
+				while (vc.isOpened() && has_next_frame) {
+					has_next_frame = vc.read(mat_capture); // Re:read
 					// get the diff.
 					Core.absdiff(mat_first, mat_capture, mat_export);
+
+					// Write
+					// mat_export.convertTo(mat_export, CvType.CV_8U);
+					vw.write(mat_export);
 
 					// convent bit size(?)
 					mat_first.convertTo(mat_first, CvType.CV_8U);
@@ -80,17 +87,12 @@ public class Main {
 
 					// What's this? -> 差分した動体の軌跡が残る程度
 					Imgproc.accumulateWeighted(mat_first, mat_export, 0.022);
-
-					// Output video
-					int codec = Integer.valueOf("MP4S");
-					double fps = 0.0;
-					Size size = new Size(1920, 1080);
-					vw.open(outputfile_path.toString(), codec, fps, size);
-					System.out.println("Output: " + outputfile_path.toString());
 				}
+				vw.release();
+				vc.release();
 			}
-			vw.release();
-			vc.release();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
